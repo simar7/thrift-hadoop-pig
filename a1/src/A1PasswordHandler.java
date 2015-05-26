49ff631df3c8cd1ed1874ab50459259c011bc3c4
Simarpreet Singh <s244sing@uwaterloo.ca>
2015-05-26 12:20:24 -0400

    Adding some basic hashing logic.

--- a1/src/A1PasswordHandler.java
+++ a1/src/A1PasswordHandler.java
@@ -1,8 +1,8 @@
 import org.apache.thrift.TException;
-import org.mindrot.jbcrypt.*;
 
 // Generated code
 import ece454750s15a1.*;
+import org.mindrot.jbcrypt.BCrypt;
 
 // TODO: Check if this is the right excpetion
 import java.lang.Exception;
@@ -14,13 +14,16 @@ public class A1PasswordHandler implements A1Password.Iface {
     }
 
     public String hashPassword(String password, short logRounds) throws ServiceUnavailableException {
-        // TODO: jBcrypt logic here.
-        // TODO: Fix the empty string to meaningful hash.
-        // return "";
 
-        // if something isn't right
-        ServiceUnavailableException SUE = new ServiceUnavailableException();
-        throw SUE;
+        String hashedString = BCrypt.hashpw(password, BCrypt.gensalt(logRounds));
+        if (hashed) {
+            return hashedString;
+        }
+        else {  // something isn't right.
+            ServiceUnavailableException SUE = new ServiceUnavailableException();
+            throw SUE;
+        }
+
     }
 
     public boolean checkPassword(String password, String hash) throws org.apache.thrift.TException {
