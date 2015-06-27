6a9a8bc1647aecf746f4d6c737c1d1a407ad5c28
Simarpreet Singh <s244sing@uwaterloo.ca>
2015-06-27 00:29:18 -0400

    Adding AdjacencyMatrix Implementation.

--- ece454750a2/src/ece454750s15a2/TriangleCountImpl.java
+++ ece454750a2/src/ece454750s15a2/TriangleCountImpl.java
@@ -2,15 +2,24 @@
  * ECE 454/750: Distributed Computing
  * <p/>
  * Code written by Wojciech Golab, University of Waterloo, 2015
- * Addendum Authors:
+ * Addendum Author(s):
  * 1) Simarpreet Singh s244sing@uwaterloo.ca
+ * Wrote:
+ *  a) Adjacency Matrix Graph representation
  * <p/>
  */
 
 package ece454750s15a2;
 
 import java.io.*;
+import java.io.IOError;
+import java.io.IOException;
+import java.lang.Integer;
+import java.lang.Iterable;
+import java.lang.RuntimeException;
 import java.util.*;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class TriangleCountImpl {
     private byte[] input;
@@ -21,6 +30,67 @@ public class TriangleCountImpl {
         this.numCores = numCores;
     }
 
+    public class AdjMatrixGraph {
+        private int V;
+        private int E;
+        private boolean[][] adjMatrix;
+
+        public AdjMatrixGraph(int V) {
+            if (V < 0) throw new RuntimeException("Number of vertices cannot be negative.");
+            this.V = V;
+            this.E = 0;
+            this.adjMatrix = new boolean[V][V];
+        }
+
+        private class AdjIterator implements Iterator<Integer>, Iterable<Integer> {
+            int v, w = 0;
+            AdjIterator(int v) { this.v = v; }
+
+            public Iterator<Integer> iterator() { return this; }
+
+            public boolean hasNext() {
+                while (w < V) {
+                    if (adjMatrix[v][w])
+                        return true;
+                    w++;
+                }
+                return false;
+            }
+
+            public Integer next() {
+                if (hasNext())
+                    return w++;
+                else
+                    throw new NoSuchElementException();
+            }
+
+            public void remove()  { throw new UnsupportedOperationException();  }
+        }
+
+        public int getVCount() {
+            return this.V;
+        }
+        public int getECount() {
+            return this.E;
+        }
+
+        public void addEdge(int x, int y) {
+            if(!this.adjMatrix[x][y]) // Only increment if new edge.
+                this.E++;
+            this.adjMatrix[x][y] = true;
+            this.adjMatrix[y][x] = true;
+        }
+
+        public boolean contains(int x, int y) {
+            return this.adjMatrix[x][y];
+        }
+
+        public Iterable<Integer> adjMatrix(int v) {
+            return new AdjIterator(v);
+        }
+
+    }
+
     public List<String> getGroupMembers() {
         ArrayList<String> ret = new ArrayList<String>();
         ret.add("s244sing");
@@ -28,13 +98,13 @@ public class TriangleCountImpl {
         return ret;
     }
 
-    public List<Triangle> enumerateTriangles() throws IOException {
+    public List<Triangle> enumerateTrianglesNaive() throws IOException {
         // this code is single-threaded and ignores numCores
 
         ArrayList<ArrayList<Integer>> adjacencyList = getAdjacencyList(input);
         ArrayList<Triangle> ret = new ArrayList<Triangle>();
 
-        // naive triangle counting algorithm
+        // naive triangle counting algorithm. 0xBOOOHISS
         int numVertices = adjacencyList.size();
         for (int i = 0; i < numVertices; i++) {
             ArrayList<Integer> n1 = adjacencyList.get(i);
@@ -50,8 +120,50 @@ public class TriangleCountImpl {
                 }
             }
         }
+        return ret;
+    }
+
+    public List<Triangle> enumerateTriangles() throws IOException {
+        // TODO: Utilize ncores and parallelize
+
+        AdjMatrixGraph adjacencyMatrix = getAdjacencyMatrix(input);
+        ArrayList<Triangle> ret = new ArrayList<Triangle>();
+
+        // TODO: Add logic to compute the trace of the matrix
 
         return ret;
+    }
+
+
+    public AdjMatrixGraph getAdjacencyMatrix(byte[] data) throws IOException {
+        System.out.println("Algorithm: Adjacency Matrix Scheme");
+
+        InputStream istream = new ByteArrayInputStream(data);
+        BufferedReader br = new BufferedReader(new InputStreamReader(istream));
+        String strLine = br.readLine();
+        if (!strLine.contains("vertices") || !strLine.contains("edges")) {
+            System.err.println("Invalid graph file format. Offending line: " + strLine);
+            System.exit(-1);
+        }
+        String parts[] = strLine.split(", ");
+        int numVertices = Integer.parseInt(parts[0].split(" ")[0]);
+        int numEdges = Integer.parseInt(parts[1].split(" ")[0]);
+        System.out.println("Found graph with " + numVertices + " vertices and " + numEdges + " edges");
+
+        AdjMatrixGraph adjMatrix = new AdjMatrixGraph(numVertices);
+
+        while((strLine = br.readLine()) != null && !strLine.equals("")) {
+            parts = strLine.split(": ");
+            int current_vertex = Integer.parseInt(parts[0]);
+            if (parts.length > 1) { // we haz edges!
+                parts = parts[1].split(" +");
+                for(String edge : parts) {
+                    adjMatrix.addEdge(current_vertex, Integer.parseInt(edge));
+                }
+            }
+        }
+
+        return adjMatrix;
     }
 
     public ArrayList<ArrayList<Integer>> getAdjacencyList(byte[] data) throws IOException {
