a3031d7fdda1336f8d2e6ad4d2abdc5beb2100da
Simarpreet Singh <simar@linux.com>
2015-07-19 12:11:04 -0400

    adding sh script to build pig, jar file
    Signed-off-by: Simarpreet Singh <simar@linux.com>

---
+++ ece454750a3/part4/src/p4p3_ccr.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+rm *.jar *.class
+CLASSPATH=$(hadoop classpath):$(find /usr/lib/pig/pig-0.14.0/pig-0.14.0-core-h2.jar -name "*.jar"):$(find /usr/hdp/current/pig-client/ -name "*.jar")
+javac -cp $CLASSPATH  SimiCalc.java
+jar -cf SimiCalc.jar SimiCalc*.class
