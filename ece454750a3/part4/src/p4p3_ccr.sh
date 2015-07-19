f283e8d500f8d3e060c835999f0cacd8204e7d65
Simarpreet Singh <simar@linux.com>
2015-07-19 12:23:39 -0400

    fixing a inputfile path issue
    Signed-off-by: Simarpreet Singh <simar@linux.com>

--- ece454750a3/part4/src/p4p3_ccr.sh
+++ ece454750a3/part4/src/p4p3_ccr.sh
@@ -4,3 +4,4 @@ rm *.jar *.class
 CLASSPATH=$(hadoop classpath):$(find /usr/lib/pig/pig-0.14.0/pig-0.14.0-core-h2.jar -name "*.jar"):$(find /usr/hdp/current/pig-client/ -name "*.jar")
 javac -cp $CLASSPATH  SimiCalc.java
 jar -cf SimiCalc.jar SimiCalc*.class
+pig -param input="../inputFolder/sampledata.txt" -param output="outputFolder" Part4_3.pig
