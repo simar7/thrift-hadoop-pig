b4697bbf80fb243a11548d9631044e89e45d1503
Simarpreet Singh <simar@linux.com>
2015-07-19 12:11:27 -0400

    get rid of the logs
    Signed-off-by: Simarpreet Singh <simar@linux.com>

--- ece454750a3/part4/src/p4p3_ccr.sh
+++ ece454750a3/part4/src/p4p3_ccr.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+rm *.log
 rm *.jar *.class
 CLASSPATH=$(hadoop classpath):$(find /usr/lib/pig/pig-0.14.0/pig-0.14.0-core-h2.jar -name "*.jar"):$(find /usr/hdp/current/pig-client/ -name "*.jar")
 javac -cp $CLASSPATH  SimiCalc.java
